51a210cafd3f0bf343f2b29f0942c80aaf4785a7
semjonG <semjon.gerasimov@gmail.com>
2021-10-09 14:40:22 +0300

    Initial Commit

---
+++ Time targets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time targets
+//
+//  Created by mac on 09.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
